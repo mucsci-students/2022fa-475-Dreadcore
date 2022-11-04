8224a68e55dbabc12a3102ba61deb615239dea38
auanmu <auan@millersville.edu>
2022-11-04 14:03:14 -0400

    try to get spider shot

--- Assets/Scripts/EnemyTurretBullet.cs
+++ Assets/Scripts/EnemyTurretBullet.cs
@@ -5,37 +5,64 @@ using UnityEngine;
 public class EnemyTurretBullet : MonoBehaviour
 {
     public GameObject player;
+    public GameObject spider;
+    private PlayerMovement3 plyrmove3;
     private Rigidbody2D rb;
     public float force;
     private float timer;
+    private float rot;
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
-        Vector3 direction = player.transform.position - transform.position;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
-        
-        
-        
-        //rotation for bullet
-        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0,0,rot);
-
+        spider = GameObject.FindGameObjectWithTag("Spider");
+        plyrmove3 = player.GetComponent<PlayerMovement3>();
     }
 
     // Update is called once per frame
     void Update()
     {
-   
+        if (!plyrmove3.spiderMode)
+        {
+        //direction to where the bullet goes to
+        direction = player.transform.position - transform.position;
+        //speed of the bullet
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,rot);
+
+        }
+        else
+        {
+            //direction to where the bullet goes to
+            direction = spider.transform.position - transform.position;
+            //speed of the bullet
+            rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+            rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0,0,rot);
+        }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            other.gameObject.GetComponent<playerDataT>().TakeDamage(1);
-            Destroy(gameObject);
+        if (!plyrmove3.spiderMode) 
+        { 
+            if (other.gameObject.CompareTag("Player"))
+            {
+                other.gameObject.GetComponent<playerDataT>().TakeDamage(1);
+                Destroy(gameObject);
+            }
         }
+        else
+        {
+            if (other.gameObject.CompareTag("Spider"))
+            {
+                other.gameObject.GetComponent<playerDataT>().TakeDamage(1);
+                Destroy(gameObject);
+            }
+        }
+            
         if (other.gameObject.CompareTag("Ground"))
         {
             Destroy(gameObject);
--- Assets/Scripts/turretShooting.cs
+++ Assets/Scripts/turretShooting.cs
@@ -6,29 +6,58 @@ public class turretShooting : MonoBehaviour
 {
     public GameObject bullet;
     public GameObject player;
+    public GameObject spider;
+    private float distance;
+    private PlayerMovement3 plyrmove3;
     public Transform bulletPos;
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        spider = GameObject.FindGameObjectWithTag("Spider");
+        plyrmove3 = player.GetComponent<PlayerMovement3>();
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        float distance = Vector2.Distance(transform.position, player.transform.position);
-        //Debug.Log(distance);
-        if (distance < 4)
+        if (!plyrmove3.spiderMode)
         {
-            timer += Time.deltaTime;
-            if (timer > 2)
+            distance = Vector2.Distance(transform.position, player.transform.position);
+        }
+        else
+        {
+            distance = Vector2.Distance(transform.position, spider.transform.position);
+        }
+        //float distance2 = Vector2.Distance(transform.position, spider.transform.position);
+        //Debug.Log(distance);
+        if (!plyrmove3.spiderMode)
+        {
+            if (distance < 4)
             {
-                timer = 0;
-                shoot();
+                timer += Time.deltaTime;
+                if (timer > 2)
+                {
+                    timer = 0;
+                    shoot();
+                }
             }
-        }    
+        }
+        else
+        {
+            if (distance < 4)
+            {
+                timer += Time.deltaTime;
+                if (timer > 2)
+                {
+                    timer = 0;
+                    shoot();
+                }
+            }
+         }
+        
     }
     void shoot()
     {
